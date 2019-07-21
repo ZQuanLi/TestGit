aeb10a7d01a2fca3b97613b0f0d40f7bd4a23fce
tom_glb <goodMorning_glb@lizequan.com>
2019-07-21 21:38:15 +0800

    edit by other branch

---
+++ src/main/java/com/lzq/Happy.java
@@ -0,0 +1,11 @@
+package com.lzq;
+
+/**
+ * @author SakuraLLL权
+ * @create 2019-07-21 21:37
+ */
+public class Happy {
+    public static void main(String[] args) {
+        System.out.println("edit by other branch");
+    }
+}
